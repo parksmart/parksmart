6f071d4c4a811ea087d9d27ad7b1eba3baa44dff
Puneet Behl <puneet.behl@tothenew.com>
2015-09-26 02:09:02 +0530

    Added rest mapping in UrlMappings.groovy

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -7,6 +7,21 @@ class UrlMappings {
             }
         }
 
+        "/api/v1.0/$controller/$id"(parseRequest: true) {
+            action = [GET: "show", PUT: "update", DELETE: "delete"]
+            constraints {
+                id(matches: /\d+/)
+            }
+        }
+
+        "/api/v1.0/$controller"(parseRequest: true) {
+            action = [GET: "list", POST: "save"]
+        }
+
+        "/api/v1.0/$controller/$action"(parseRequest: true)
+
+        "/api/v1.0/$controller/$action/$id"(parseRequest: true)
+
         "/"(view:"/index")
         "500"(view:'/error')
 	}
