76c4650c18a59e32115681fea69baae553aedfaf
Puneet Behl <puneet.behl@tothenew.com>
2015-09-26 01:41:51 +0530

    Created domain Advertisement

---
+++ test/unit/park/smart/AdvertisementSpec.groovy
@@ -0,0 +1,20 @@
+package park.smart
+
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+
+/**
+ * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
+ */
+@TestFor(Advertisement)
+class AdvertisementSpec extends Specification {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+    }
+}
