b3de4e94ce2aa7a40cf4e02d9b3b27f31d3d4730
Puneet Behl <puneet.behl@tothenew.com>
2015-09-27 02:16:23 +0530

    Added REST support for Bookings.

---
+++ grails-app/controllers/com/parksmart/BookingController.groovy
@@ -0,0 +1,109 @@
+package com.parksmart
+
+import grails.rest.RestfulController
+
+import static org.springframework.http.HttpStatus.*
+import grails.transaction.Transactional
+
+@Transactional(readOnly = true)
+class BookingController extends RestfulController {
+
+    static responseFormats = ['html', 'json', 'xml']
+    static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
+
+    BookingController() {
+        super(Booking)
+    }
+
+    def index(Integer max) {
+        params.max = Math.min(max ?: 10, 100)
+        respond Booking.list(params), model:[bookingInstanceCount: Booking.count()]
+    }
+
+    def show(Booking bookingInstance) {
+        respond bookingInstance
+    }
+
+    def create() {
+        respond new Booking(params)
+    }
+
+    @Transactional
+    def save(Booking bookingInstance) {
+        if (bookingInstance == null) {
+            notFound()
+            return
+        }
+
+        if (bookingInstance.hasErrors()) {
+            respond bookingInstance.errors, view:'create'
+            return
+        }
+
+        bookingInstance.save flush:true
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.created.message', args: [message(code: 'booking.label', default: 'Booking'), bookingInstance.id])
+                redirect bookingInstance
+            }
+            '*' { respond bookingInstance, [status: CREATED] }
+        }
+    }
+
+    def edit(Booking bookingInstance) {
+        respond bookingInstance
+    }
+
+    @Transactional
+    def update(Booking bookingInstance) {
+        if (bookingInstance == null) {
+            notFound()
+            return
+        }
+
+        if (bookingInstance.hasErrors()) {
+            respond bookingInstance.errors, view:'edit'
+            return
+        }
+
+        bookingInstance.save flush:true
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.updated.message', args: [message(code: 'Booking.label', default: 'Booking'), bookingInstance.id])
+                redirect bookingInstance
+            }
+            '*'{ respond bookingInstance, [status: OK] }
+        }
+    }
+
+    @Transactional
+    def delete(Booking bookingInstance) {
+
+        if (bookingInstance == null) {
+            notFound()
+            return
+        }
+
+        bookingInstance.delete flush:true
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.deleted.message', args: [message(code: 'Booking.label', default: 'Booking'), bookingInstance.id])
+                redirect action:"index", method:"GET"
+            }
+            '*'{ render status: NO_CONTENT }
+        }
+    }
+
+    protected void notFound() {
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.not.found.message', args: [message(code: 'booking.label', default: 'Booking'), params.id])
+                redirect action: "index", method: "GET"
+            }
+            '*'{ render status: NOT_FOUND }
+        }
+    }
+}
