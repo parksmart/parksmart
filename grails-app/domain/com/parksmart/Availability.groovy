e28512d2b564df74ecc526a6530ad46bacdefaab
Puneet Behl <puneet.behl@tothenew.com>
2015-09-26 19:36:08 +0530

    Created Advertisement domain.

--- grails-app/domain/com/parksmart/Availability.groovy
+++ grails-app/domain/com/parksmart/Availability.groovy
@@ -1,9 +1,29 @@
 package com.parksmart
 
+import grails.mongodb.geo.Point
+
 class Availability {
 
     Long advertisementId
     Date date
+    List<Double> location
+    Point geoLocation
     AvailabilityType type
+
+
+    static constraints = {
+        advertisementId()
+        date()
+        geoLocation(nullable: true)
+        location()
+        type()
+    }
+
+    static mapWith = "mongo"
+
+    static mapping = {
+        location geoIndex: '2d', indexAttributes: [min: -500, max: 500]
+        geoLocation geoIndex: '2dsphere', indexAttributes: [min: -500, max: 500]
+    }
 
 }
