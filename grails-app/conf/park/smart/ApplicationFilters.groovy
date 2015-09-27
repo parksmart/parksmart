f71859594c831fa39ed7db8c8b49dda10277a265
Puneet Behl <puneet.behl@tothenew.com>
2015-09-27 07:30:51 +0530

    Added ApplicationFilters.

--- grails-app/conf/park/smart/ApplicationFilters.groovy
+++ grails-app/conf/park/smart/ApplicationFilters.groovy
@@ -1,10 +1,13 @@
 package park.smart
 
+import grails.util.Environment
+
 class ApplicationFilters {
 
     def filters = {
         all(controller:'*', action:'*') {
             before = {
+                String envName = Environment.current.name
                 if (["qa", "development"]?.contains(envName)) {
                     log.info "Logs: >> ${params}"
                 }
