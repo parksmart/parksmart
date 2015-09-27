dce37624dafbec3efe7b9464d79b998885a824ad
Manish <manish@intelligrape.com>
2015-09-27 10:30:57 +0530

    Ensured  that  UserRole is created when a new user registers.

--- grails-app/services/com/parksmart/UserService.groovy
+++ grails-app/services/com/parksmart/UserService.groovy
@@ -5,6 +5,8 @@ class UserService {
     User registerUser(RegisterCO registerCO){
         User user = User.getInstance(registerCO)
         user.save(flush:true, failOnError: true)
+        Role role = Role.findByAuthority('ROLE_USER')
+        UserRole.create(user, role, true )
         user
     }
 
